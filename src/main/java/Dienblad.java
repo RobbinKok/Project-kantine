ceaabf5e2abf8dbc26a7d0046c1d9423f2fb3289
robbinkok01@gmail.com <robbinkok01@gmail.com>
2020-05-13 19:08:04 +0200

    The methods now do what they should do and added a klant to the dienblad, as per 1.5

--- src/main/java/Dienblad.java
+++ src/main/java/Dienblad.java
@@ -4,12 +4,22 @@ import java.util.Stack;
 
 public class Dienblad {
     private ArrayList<Artikel> artikelen;
-
+    Persoon klant;
     /**
      * Constructor
      */
     public Dienblad() {
         // method body omitted
+        artikelen = new ArrayList<>();
+        klant = new Persoon();
+    }
+
+    /**
+     * De constructor die een nieuwe dienbald aanmaakt met klant als parameter
+     * @param klant
+     */
+    public Dienblad(Persoon klant) {
+        this.klant = klant;
     }
 
     /**
@@ -19,6 +29,7 @@ public class Dienblad {
      */
     public void voegToe(Artikel artikel) {
         // method body omitted
+        artikelen.add(artikel);
     }
 
     /**
@@ -28,6 +39,8 @@ public class Dienblad {
      */
     public int getAantalArtikelen() {
         // method body omitted
+
+        return artikelen.size();
     }
 
     /**
@@ -37,6 +50,23 @@ public class Dienblad {
      */
     public double getTotaalPrijs() {
         // method body omitted
+
+        float prijs = 0;
+        for (Artikel a: artikelen) {
+            a.getPrijs();
+            prijs = prijs + a.getPrijs();
+
+        }
+        return prijs;
     }
+
+    public Persoon getKlant() {return klant;}
+
+
+    public String getKlantNaam() { return klant.getVoornaam() + " " + klant.getAchternaam(); }
+
+    public String getKlantGeslacht() { return klant.getGeslacht(); }
+
+    public int getKlantBSN() { return klant.getBsn();}
 }
 
