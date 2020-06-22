572ddc34d407fb933a577c23b004e4516cbfea80
RobbinKok <robbinkok01@gmail.com>
2020-06-22 20:02:54 +0200

    Added an EntityManager to FactuurRegel, per 6.4A

--- src/main/java/FactuurRegel.java
+++ src/main/java/FactuurRegel.java
@@ -1,11 +1,18 @@
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "FactuurRegel")
 public class FactuurRegel implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @ManyToOne(targetEntity = Factuur.class)
     private Factuur factuur;
 
+    @Column(name = "Artikel", nullable = false)
     private Artikel artikel;
 
     public FactuurRegel(){
