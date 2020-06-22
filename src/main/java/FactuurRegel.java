8a00cec8558e7cbe500ccce62157e360760d6be7
RobbinKok <robbinkok01@gmail.com>
2020-06-22 19:46:08 +0200

    Added the calss FactuurRegel

---
+++ src/main/java/FactuurRegel.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+public class FactuurRegel implements Serializable {
+
+    private long id;
+
+    private Factuur factuur;
+
+    private Artikel artikel;
+
+    public FactuurRegel(){
+
+    }
+
+    public FactuurRegel(Factuur factuur, Artikel artikel){
+        this.factuur = factuur;
+        this.artikel = artikel;
+    }
+
+    /**
+     * @return een printbare factuurregel
+     */
+    public String toString(){
+        // method body omitted
+    }
+
+}
